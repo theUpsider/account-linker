a38cda80997996bccf545c684bded7a079665893
BuildTools <unconfigured@null.spigotmc.org>
2022-02-11 14:59:19 +0000

    config and template

---
+++ commands/verify.js
@@ -0,0 +1,238 @@
+const Keyv = require("keyv");
+const settings = require("../general-settings.json");
+const discord = require("discord.js");
+const { mailpw } = require("../config.json");
+const { ValidateEmail, logMessage } = require("../functions/extensions.js");
+const MailPw = mailpw; // prevent on demand loading
+var Imap = require("imap");
+// Mail https://github.com/mscdex/node-imap
+var imap;
+
+module.exports = {
+  name: "verify",
+  admin: false,
+  description: "verifies your email adress",
+  args: true,
+  guildOnly: true,
+  usage: "<student mail>",
+  async execute(message, args) {
+    // get member from guild the message was sent in
+    const memberToAdd = await message.guild.members.fetch(message.author.id);
+
+    logMessage(message, `${memberToAdd} tries to verify...`);
+
+    try {
+      imap = new Imap({
+        user: "info@akgaming.de",
+        password: MailPw,
+        host: "imap.ionos.de",
+        port: 993,
+        tls: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const mailArg = args[0];
+    var mailFound = false;
+
+    // check mail validity
+    if (!ValidateEmail(mailArg, message)) {
+      message.delete({ timeout: 1000 });
+      return;
+    }
+
+    // first log in to mail
+    imap.once("ready", async function () {
+      imap.openBox("INBOX", true, async function (error, box) {
+        if (error) console.log("Error in: ", box, " error; ", error);
+        logMessage(
+          message,
+          `Username: ${message.author.username} tries to verify`
+        );
+        // search for discord name in INBOX
+        imap.search(
+          [["HEADER", "SUBJECT", message.author.username]],
+          function (err, results) {
+            console.log(results);
+            if (err) throw err;
+            if (
+              results === undefined ||
+              results === null ||
+              (Array.isArray(results) && results.length === 0)
+            ) {
+              logMessage(message, "Nothing to found.");
+              message.reply(`No mail with ${message.author.username} arrived.`);
+              return;
+            }
+            var f = imap.fetch(results, {
+              bodies: "HEADER.FIELDS (FROM TO SUBJECT DATE)",
+            });
+            f.on("message", function (msg, seqno) {
+              msg.on("body", function (stream, info) {
+                if (info.which === "TEXT") var buffer = "";
+                //write data into buffer
+                stream.on("data", function (chunk) {
+                  buffer += chunk.toString("utf8");
+                });
+                //handle data
+                stream.once("end", async function () {
+                  //in case there are pre existing mails, skip the process
+                  if (!mailFound) await registerMember(info, buffer, message);
+                  mailFound = true;
+                });
+              });
+            });
+          }
+        );
+
+        // remove message so others dont see it
+        try {
+          message.delete({ timeout: 4000 });
+        } catch (error) {
+          logMessage(message, error);
+        }
+      });
+    });
+
+    try {
+      imap.connect();
+      console.log("connected");
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  },
+};
+
+async function registerMember(info, buffer, message) {
+  if (info.which !== "TEXT") {
+    if (!Imap.parseHeader(buffer).undefinedfrom[0]) {
+      logMessage(
+        message,
+        `Wrong header ${Imap.parseHeader(buffer).undefinedfrom[0]}`
+      );
+    }
+
+    const from = Imap.parseHeader(buffer).undefinedfrom[0];
+    const endmail = from.split(`@`)[1].split(`>`)[0];
+
+    // is student
+    if (endmail.toString().includes("stud.hs-kempten.de")) {
+      // TODO if something failed, answer mail whats wrong!
+      // Key: student-email, Value: verification date
+      const dbverify = new Keyv("sqlite://verify.sqlite");
+      dbverify.on("error", (err) =>
+        console.error("Keyv connection error:", err)
+      );
+      // Key: student-email, Value: discord-id
+      const db_map_emailToId = new Keyv("sqlite://map_emailToId.sqlite");
+      db_map_emailToId.on("error", (err) =>
+        console.error("Keyv connection error:", err)
+      );
+      // parse mail
+      try {
+        const fromMailDiscordId = await db_map_emailToId.get(from);
+        const verifymailDate = await dbverify.get(from);
+        const MailUsername = Imap.parseHeader(buffer).subject[0].split("#")[0];
+        console.log(`Mail subject: `, Imap.parseHeader(buffer).subject);
+
+        // get member from guild the message was sent in
+        const memberToAdd = await message.guild.members.fetch(
+          message.author.id
+        );
+        // TODO check if id not present! could log in as another user and use verification of real student mail
+        // if mail matches the username and ID not present do verification
+        if (MailUsername === message.author.username)
+          await addMember(
+            from,
+            memberToAdd,
+            MailUsername,
+            dbverify,
+            db_map_emailToId
+          );
+        else {
+          logMessage(
+            message,
+            `${message.author.username} send another username via mail: ${MailUsername}. Mistake or trying to let someone else in?`
+          );
+          message.reply(
+            `You tried to verify a wrong username: ${MailUsername}. Yours is: ${message.author.username}`
+          );
+        }
+
+        if (!verifymailDate || verifymailDate === undefined) {
+          // if mail is registered and new discord user in mail -> impostor!
+          console.log("Newbie. First Server!");
+        } else if (
+          memberToAdd.roles.cache.has(
+            memberToAdd.guild.roles.cache.find(
+              (role) => role.name === settings.roles.verified
+            ).id
+          )
+        ) {
+          console.log("already verifed user tried to send mail again: ", from);
+          message.reply("You are already verified.");
+        } else if (verifymailDate || verifymailDate !== undefined) {
+          logMessage(
+            message,
+            `${message.author.username} user tried to verify again, although having no role. Possibly was on other faculty before.`
+          );
+        }
+      } catch (error) {
+        console.log(error);
+        logMessage(
+          message,
+          `displayname not found in server. User probably sent wrong name.`
+        );
+      }
+    } else {
+      logMessage(
+        message,
+        `${message.author.username} send an email from a non-student adress. Maybe dig into this @${settings.roles.staffrole}.`
+      );
+      message.reply("You sent the verification mail from a non-student email.");
+    }
+  }
+
+  async function addMember(
+    from,
+    memberToAdd,
+    displayName,
+    dbverify,
+    db_map_emailToId
+  ) {
+    logMessage(message, `Granted rank student: ${memberToAdd}`);
+
+    if (
+      !memberToAdd.roles.cache.has(
+        memberToAdd.guild.roles.cache.find(
+          (role) => role.name === settings.roles.verified
+        ).id
+      )
+    ) {
+      message.reply(
+        "You should be verified instantly. If you dont see any channels within 5 seconds, something went wrong. Someone will dig into this."
+      );
+
+      //notify user in DM with steps
+      memberToAdd.send(`
+									**Herzlich Willkommen auf dem Discord ${memberToAdd.guild.name}!**\nNachfolgend findest Du eine kurze Beschreibung, wie du dich auf unserem Server zurecht findest.\nGenerell ist jeder Studierende berechtigt alle *Kanäle* für jedes Fach, oder jeden Studiengang in der Fakultät einzusehen.\nAber um das Chaos zu minimieren, dienen *Rollen* als eine Art **Filter**, um Dich vor der Flut an Kanälen zu bewahren. Deshalb kannst Du in\n**"rollenanfrage"** sowie **"react-a-role"** dein Semester auswählen, bzw. abwählen. Danach siehst Du die Fächer, die für Dich relevant sind!\nJedes Semester enthält Kategorien, in denen Du Dich mit anderen austauschen kannst.\nEs gibt ein paar semesterübergreifende Kategorien, wie **"/ALL"** und **"WICHTIGES"**.\nDort im Kanal **"ankündigungen"** kommen regelmäßige News zu hochschulweiten Veranstaltungen oder Events, sowie Erungenschaften und nice to knows.\n\nBitte lies Dir den **"rules"** Kanal durch, damit du weißt wie wir auf Discord miteinander umgehen.\nSolltest Du noch Fragen haben, stell sie direkt im **"fragen"** channel oder kontaktiere einen **Administrator/Owner/Moderator** rechts in der Mitgliederliste.\n\nVielen Dank, dass Du dabei bist, **${displayName}!**\n`);
+      await dbverify.set(from, Date.now());
+      await db_map_emailToId.set(from, memberToAdd.user.id);
+    }
+
+    try {
+      // add role
+      memberToAdd.roles.add(
+        // get role from guild chache
+        memberToAdd.guild.roles.cache.find(
+          (role) => role.name === settings.roles.verified
+        ).id
+      );
+    } catch (UnhandledPromiseRejectionWarning) {
+      console.log("Missing access to role management.");
+      return;
+    }
+  }
+}
---
+++ index.js
@@ -0,0 +1,319 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const Canvas = require("canvas");
+const fetch = require("node-fetch");
+const Keyv = require("keyv");
+const sqlite3 = require("sqlite3").verbose();
+var RiotRequest = require("riot-lol-api");
+const { prefix, token } = require("./config.json");
+const { toLevel } = require("./functions/extensions.js");
+const settings = require("./general-settings.json");
+const fs = require("fs");
+inspect = require("util").inspect;
+
+const bot = new Discord.Client();
+
+// cooldowns
+const cooldowns = new Discord.Collection();
+// commands
+bot.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js")); // read file with commands
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  bot.commands.set(command.name, command); // with the key as the command name and the value as the exported module
+}
+
+//db stuff
+let db = new sqlite3.Database(":memory:", (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Connected to the in-memory SQlite database.");
+});
+
+//--------------------------------------------------
+//                    DATABASE STUFF 
+//--------------------------------------------------
+// Key: discord ID, Value: xp value
+const dbxp = new Keyv("sqlite://xp.sqlite"); // const keyv = new Keyv(); // for in-memory storage //
+dbxp.on("error", (err) => console.error("Keyv connection error:", err));
+// Key: student-email, Value: verification date
+const dbverify = new Keyv("sqlite://verify.sqlite");
+dbverify.on("error", (err) => console.error("Keyv connection error:", err));
+//--------------------------------------------------
+//                    BOT   LOGIN 
+//--------------------------------------------------
+bot.login(token);
+
+bot.on("ready", () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+  bot.user.setActivity("use --help", { type: "PLAYING" });
+});
+//--------------------------------------------------
+//                  MESSAGE
+//--------------------------------------------------
+bot.on("message", async (message) => {
+  if (message.author.bot) return;
+
+  // no command! - simple message to track for XP
+  if (message.content.startsWith(`verify`))
+    message.reply("You need to use ..verify");
+  if (!message.content.startsWith(prefix) && message.channel.type == "text") {
+    // handle ads
+    if (message.channel.name == settings.channels.ads) {
+      // TODO extend to database + calculation for the case the bot crashes
+      var deletionDate = Date.now();
+      deletionDate.setMilliseconds(
+        deletionDate.getMilliseconds() + settings.settings.adstimeout
+      );
+      console.info(`ad posted. Will be deleted on ${deletionDate}`);
+      message.delete({ timeout: settings.settings.adstimeout }); // 4 weeks
+    }
+
+    const userXP = await dbxp.get(message.author.id);
+
+    // user xp
+    if (!userXP || userXP === undefined) {
+      await dbxp.set(message.author.id, 1); // set to 1 for 1 XP
+      return;
+    } else {
+      // if new level, post XP
+      if (Math.trunc(toLevel(userXP + 1)) > Math.trunc(toLevel(userXP))) {
+        // send level xp to xp channel
+        const canvas = Canvas.createCanvas(700, 250);
+        const ctx = canvas.getContext("2d");
+
+        // Since the image takes time to load, you should await it
+        const background = await Canvas.loadImage("./images/banner.png");
+        // This uses the canvas dimensions to stretch the image onto the entire canvas
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        // Select the color of the stroke
+        ctx.strokeStyle = "#74037b";
+        // Draw a rectangle with the dimensions of the entire canvas
+        ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+        // Select the font size and type from one of the natively available fonts
+        ctx.font = "60px sans-serif";
+
+        // Slightly smaller text placed above the member's display name
+        ctx.font = applyText(canvas, `${message.author.username} has reached`);
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(
+          `${message.author.username} has reached`,
+          canvas.width / 2.4,
+          canvas.height / 3.5
+        );
+
+        // Add an exclamation point here and below
+        ctx.font = applyText(
+          canvas,
+          `LEVEL ${Math.trunc(toLevel(userXP + 1))}`
+        );
+        ctx.fillStyle = "#ffffff";
+        ctx.fillText(
+          `LEVEL ${Math.trunc(toLevel(userXP + 1))}`,
+          canvas.width / 2.4,
+          canvas.height / 1.5
+        );
+
+        // Use helpful Attachment class structure to process the file for you
+        const attachment = new Discord.MessageAttachment(
+          canvas.toBuffer(),
+          "level-up-image.png"
+        );
+        (
+          await message.guild.channels.cache
+            .find((channel) => channel.name == settings.channels.xp)
+            .fetch()
+        ).send(`Congrats, ${message.author}!`, attachment);
+      }
+
+      // New 1 XP for 200 chars. That means 200 chars equals to one XP Point
+      dbxp.set(message.author.id, userXP + (message.content.length / (parseFloat(settings.settings.CharsForLevel))));
+    }
+
+    return;
+  }
+
+  //filter args
+  const args = message.content.slice(prefix.length).split(/ +/); //filter args
+  const commandName = args.shift().toLowerCase();
+
+  //command checking aliases
+  const command =
+    bot.commands.get(commandName) ||
+    bot.commands.find(
+      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+    );
+
+  if (!command) return;
+
+  //error checking
+  //guild check
+  if (command.guildOnly && message.channel.type !== "text") {
+    return message.reply("I can't execute that command inside DMs!");
+  }
+
+  // Staff check
+  if (command.admin && !message.member.roles.cache.has(
+        message.member.guild.roles.cache.find(
+          (role) => role.name === settings.roles.staffrole
+        ).id
+      )) {
+    return message.reply("You are not alled to execute this command!");
+  }
+
+  //args check
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments, ${message.author}!`;
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    }
+    return message.channel.send(reply);
+  }
+
+  // cooldown
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 1) * 1000;
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(
+        `please wait ${timeLeft.toFixed(1).toHHMMSS()} before reusing the \`${
+          command.name
+        }\` command.`
+      );
+    }
+  } else {
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  }
+
+  // ----------------------------------------------------------------------------------------------------------------------------------------------
+  // Execute command
+  // ----------------------------------------------------------------------------------------------------------------------------------------------
+  try {
+    // check for reload!
+    if(command.name === "reload" || "r")
+      command.execute(message,args, bot) // requires special arg
+    else
+      command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
+
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+// User Join
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+bot.on("guildMemberAdd", (member) => {
+  // const embeded = new Discord.MessageEmbed()
+  // 	.setColor(settings.colors.blue)
+  // 	.setTitle(`A new user has joined`)
+  // 	.setDescription(`Welcome **` + member.user.username + `** to the tournament server! Before doing anything else read <#${settings.channels.information}> and <#${settings.channels.rules}>. Any further questions should be directed towards our staff. Enjoy your stay!`)
+  // 	.setFooter(settings.footer);
+  // try {
+  // 	member.guild.channels.resolve(settings.channels.greetings).send(embeded);
+  // } catch (error) {
+  // 	console.log(error)
+  // }
+});
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+// User Voice channel interaction
+// ----------------------------------------------------------------------------------------------------------------------------------------------
+bot.on('voiceStateUpdate', (oldState, newState) => {
+    let newUserChannelName = null;
+    if(newState.channel !== null){
+        newUserChannelName = newState.channel.name
+    }
+    let oldUserChannelName = null
+    if(oldState.channel !== null){
+        oldUserChannelName = oldState.channel.name
+    }
+
+    // When a create Channel has been clicked
+    if(newUserChannelName === settings.channels.createChannel)
+    {
+      if(oldUserChannelName=== '🔊 '+oldState.member.displayName)
+      oldState.channel.delete()
+
+      newChannel = newState.guild.channels.create('🔊 '+newState.member.displayName, {
+        type: 'voice',
+        parent: newState.channel.parent,
+      }).then(function(result){
+        newState.member.voice.setChannel(result)
+      })
+      // Move creator in his new channel
+      //newState.member.voice.setChannel(newChannel)
+
+    // If creator leaves channel, delete it
+    }else if(oldUserChannelName=== '🔊 '+oldState.member.displayName){
+      oldState.channel.delete()
+    }
+});
+
+// extended functionality
+// ------------------------
+String.prototype.toHHMMSS = function () {
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - hours * 3600) / 60);
+  var seconds = sec_num - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return hours + "h " + minutes + "min " + seconds + "sec";
+};
+
+function attachIsImage(msgAttach) {
+  var url = msgAttach.url;
+  //True if this url is a png image.
+  return (
+    url.indexOf("png", url.length - "png".length /*or 3*/) ||
+    url.indexOf("jpg", url.length - "jpg".length /*or 3*/) ||
+    url.indexOf("jpeg", url.length - "jpeg".length /*or 3*/) !== -1
+  );
+}
+
+async function updateBadges() {
+  const channel = bot.channels.find("name", "badge");
+  const { file } = await fetch("../exchange/users.json").then((response) =>
+    response.json()
+  );
+  channel.send(file);
+}
+
+// Pass the entire Canvas object because you'll need to access its width, as well its context
+const applyText = (canvas, text) => {
+  const ctx = canvas.getContext("2d");
+
+  // Declare a base size of the font
+  let fontSize = 70;
+
+  do {
+    // Assign the font to the context and decrement it so it can be measured again
+    ctx.font = `${(fontSize -= 10)}px sans-serif`;
+    // Compare pixel width of the text to the canvas minus the approximate avatar size
+  } while (ctx.measureText(text).width > canvas.width - 300);
+
+  // Return the result to use in the actual canvas
+  return ctx.font;
+};
